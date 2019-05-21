051d5544e389a9827ed3d810a54e3e3656d7b986
Felipe Palacios <felipeyunier@gmail.com>
2019-05-21 00:50:37 -0300

    first commit

---
+++ ZuulProxyServer/src/main/java/com/zuul/proxy/server/StackWebSecurityConfigurerAdapter.java
@@ -0,0 +1,14 @@
+package com.zuul.proxy.server;
+
+//@Configuration
+//@EnableWebSecurity
+//public class StackWebSecurityConfigurerAdapter  extends WebSecurityConfigurerAdapter{
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception{
+//        http.authorizeRequests().antMatchers("/").permitAll();
+//    }
+//}
+
+
+public class StackWebSecurityConfigurerAdapter  {
+ }
